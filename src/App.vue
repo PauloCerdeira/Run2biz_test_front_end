3e192a3de34bb644b7e4c0bbac05082242e8d897
Paulo Cerdeira <paulocerdeira1999@gmail.com>
2023-06-08 13:30:42 -0300

    finished

--- src/App.vue
+++ src/App.vue
@@ -1,68 +1,106 @@
 <template>
   <div>
-    <div>
-      <h1>Question 1 - Find Possible Passwords</h1>
-      <h2>Pattern rules:</h2>
-      <h3>Part 1</h3>
-      <ul>
-        <li>Password must be between 184759-856920.</li>
-        <li>At least two adjacent digits must be the same.</li>
-        <li>
-          Starting from left to right, digits only increase or stay the same.
-        </li>
-      </ul>
-      <h3>Part 2</h3>
-      <ul>
-        <li>
-          You must have at least one group of exactly two of the same
-          characters.
-        </li>
-      </ul>
+    <h1>Question 1 - Find Possible Passwords</h1>
+    <span>
+      You are a security expert attempting to break into a company which paid
+      you to do so. By exploiting security flaws you discover that valid
+      passwords must follow a certain pattern.
+    </span>
+    <h2>Pattern rules:</h2>
 
+    <h3>Part 1</h3>
+    <ul>
+      <li>Password must be between 184759-856920.</li>
+      <li>At least two adjacent digits must be the same.</li>
+      <li>
+        Starting from left to right, digits only increase or stay the same.
+      </li>
+    </ul>
+    <h3>Part 2</h3>
+    <ul>
+      <li>
+        You must have at least one group of exactly two of the same characters.
+      </li>
+    </ul>
+
+    <div style="text-align: center">
       <p>Choose ruleset:</p>
      
       <input v-model="exactlyOneGroup" type="radio" name="exactlyOneGroup" value="0" />
-        <label for="html">Part 1</label><br />
+        <label for="html">Part 1</label>
      
       <input v-model="exactlyOneGroup" type="radio" name="exactlyOneGroup" value="1" />
-        <label for="css">Part 2</label><br />
-     
+        <label for="css">Part 2</label>
       <br />
-      <button @click="findPossiblePasswords()">find possible passwords</button>
-      <span v-if="PossiblePasswords.count">
-        Possible Passwords: {{ PossiblePasswords.count }}</span>
-      <br />
-      <textarea id="passwordOutput" readonly rows="10" cols="30">
-Passwords Output</textarea>
     </div>
 
-    <div>
-      <h1>Question 2 - Calculate Address</h1>
+    <div class="button-div">
+      <button @click="findPossiblePasswords()">find possible passwords</button>
+      <span>
+        Possible Passwords:
 
-      <span>On a USB drive you've found a document with a large list of numbers and
-        in several different places you've found clues as to what the numbers
-        mean. Each line represents a command.
+        <span v-if="PossiblePasswords.count">
+          {{ PossiblePasswords.count }}</span>
       </span>
-      <h2>Rules:</h2>
+    </div>
 
-      <ul>
-        <li>Commands starting with 20 increase the "address" variable.</li>
-        <li>Commands starting with 5 jump to another command.</li>
-        <li>
-          Commands starting with any other digit do nothing and the next command is executed in succession.
-        </li>
-      </ul>
+    <div style="display: flex; justify-content: center">
+      <textarea id="passwordOutput" readonly rows="10" cols="30">
+  Passwords Output</textarea>
+    </div>
+  </div>
 
-      <textarea v-model="commands" rows="10" cols="30"> </textarea> <br />
+  <div>
+    <h1>Question 2 - Calculate Address</h1>
+
+    <span>On a USB drive you've found a document with a large list of numbers and
+      in several different places you've found clues as to what the numbers
+      mean. Each line represents a command.
+    </span>
+    <h2>Rules:</h2>
+
+    <div class="address-rules">
+      <div>
+        <ul>
+          <li>Commands starting with 20 increase the "address" variable.</li>
+          <li>Commands starting with 5 jump to another command.</li>
+          <li>
+            Commands starting with any other digit do nothing and the next
+            command is executed in succession.
+          </li>
+        </ul>
+      </div>
+
+      <div>
+        <textarea v-model="commands" rows="10" cols="30"> </textarea>
+      </div>
+    </div>
+
+    <div class="button-div">
       <button @click="CalcAddress()">Calculate Address</button>
-      <span v-if="CalcAddressReturn">
-        Calculated Address: {{ CalcAddressReturn.address }}</span>
-      <br />
-      <div v-if="CalcAddressReturn">
-        <div :style="'color: ' + historyColoring(step)" v-for="step in CalcAddressReturn.history" :key="step.index">
-          {{ step.command }} - 
-          <span v-if="step.command == '5'"> Skip {{ step.value }} commands </span>
-          <span v-else-if="step.command == '20'"> increase Address by {{ step.value }} </span>
+      <span>
+        Calculated Address:
+        <span v-if="CalcAddressReturn">
+          {{ CalcAddressReturn.address }}
+        </span>
+      </span>
+    </div>
+
+    <div style="display: flex; justify-content: center" v-if="CalcAddressReturn">
+      <div>
+        <div :style="historyColoring(step)" v-for="step in CalcAddressReturn.history" :key="step.index">
+          <div style="display: inline-block; min-width: 28px">
+            {{ step.command }}
+          </div>
+          <div style="display: inline-block; min-width: 15px">
+            -
+          </div>
+          <span v-if="step.command == '5'">
+            Skip {{ step.value }} commands
+          </span>
+          <span v-else-if="step.command == '20'">
+            increase Address by {{ step.value }}
+          </span>
           <span v-else-if="step.value == 'Skipped'">{{ step.value }}</span>
           <span v-else> Invalid </span>
         </div>
@@ -107,34 +145,107 @@ export default {
         this.PossiblePasswords.list;
     },
     async CalcAddress() {
-      this.CalcAddressReturn =(
+      this.CalcAddressReturn = (
         await axios.get("http://localhost:3000/calcAddress", {
           params: { commands: this.commands },
         })
-      ).data
+      ).data;
       console.log(this.CalcAddressReturn);
     },
     historyColoring(step) {
       if (step.value == "Skipped") {
-        return "Red"
-      }else if (step.command == "5") {
-        return "orange"
+        return "color: red; text-decoration: line-through;";
+      } else if (step.command == "5") {
+        return "color: orange";
       } else if (step.command == "20") {
-        return "lightGreen"
-      } else return "Grey"
-    }
+        return "color: lightGreen";
+      } else return "color: Grey";
+    },
   },
 };
 </script>
 
 <style scoped>
-main {
+h1 {
+  color: white;
+}
+
+h2 {
+  color: rgb(223, 223, 223);
+}
+
+#app {
   padding: 10px;
-  /* background-color: rgba(0, 0, 255, 0.2); */
-  height: 100%;
+  display: flex;
+}
+
+#app>div {
+  width: 100%;
+  max-width: 500px;
+  margin: 0px 15px;
+}
+
+@media (max-width: 1100px) {
+  #app>div {
+    width: 100%;
+    margin: 0px auto 20px auto;
+  }
+}
+
+.address-rules {
+  display: flex;
+}
+
+.address-rules>div:nth-child(2) {
+  min-width: 130px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+
+.button-div {
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+}
+
+.button-div>span {
+  color: white;
+  vertical-align: middle;
+  font-size: 1.2rem;
+}
+
+button {
+  cursor: pointer;
+  padding: 10px 5px;
+  background-color: #864879;
+  color: white;
+  border-radius: 12px;
+  border: 2px solid #583050;
+  margin: 5px 0px;
+  transition: transform 0.3s;
+}
+
+button:hover {
+  transform: scale(1.08);
+}
+
+button:active {
+  opacity: 0.7;
+  transform: scale(0.9);
 }
 
 textarea {
   resize: none;
+  border: 2px solid black;
+  background-color: #00000073;
+  color: white;
+  font-family: verdana;
+  border-radius: 6px;
+  padding: 10px 0px 0px 10px;
+
+}
+textarea:focus-visible {
+    outline: -webkit-focus-ring-color auto 0px;
 }
 </style>
